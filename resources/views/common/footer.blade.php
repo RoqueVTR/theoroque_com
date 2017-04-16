2461ad2ea78bf47a507551701947dd8c4fb754f0
Vincent Theo Roque <theo.roque@newmeridian.co>
2017-04-16 20:06:43 +0800

    Included temporary profile images; adjusted size of footer social media and top button.

--- resources/views/common/footer.blade.php
+++ resources/views/common/footer.blade.php
@@ -43,7 +43,7 @@
                 <div class="col-md-2">
                     <h5 class="title">Follow me</h5>
                     <ul>
-                        <li>
+                        <li class="h5">
                             <a target="_blank" href="//facebook.com/theomazing.vtr" class="badge light-blue darken-4"><i class="fa fa-facebook"></i></a>&nbsp;
                             <a target="_blank" href="//twitter.com/_theomazing" class="badge blue"><i class="fa fa-twitter"></i></a>&nbsp;
                             <a target="_blank" href="//www.linkedin.com/in/vincent-theo-roque-39709b9b/" class="badge light-blue darken-3"><i class="fa fa-linkedin"></i></a>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="stylesheet" type="text/css" href="/css/app.css">
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="/images/theoroque-favicon.psd">
 
     <script>
         window.Laravel = {!! json_encode([
