99755e07bc2e9632e981264d669c27b0709ed8eb
Vincent Theo Roque <theo.roque@newmeridian.co>
2017-04-18 20:21:13 +0800

    Removed Aaron temporarily

---
+++ resources/views/aaron.blade.php
@@ -0,0 +1,17 @@
+<div class="col-sm-4">
+ <!--Card image-->
+    <div class="hm-zoom testimonial-image-container mx-auto">
+        <img src="/images/testimonials/aaron.jpg" class="img-fluid testimonial-image z-depth-3" alt="Aaron Lovelace">
+    </div>
+    <!--/.Card image-->
+    <div class="card-block">
+        <div class="card-title">
+            <h4><strong>Aaron Lovelace</strong></h4>
+            <h6>Beestripe LLC</h6>
+            <h6>Co-founder / Board Member</h6>
+        </div>
+        <div class="card-text testimonial-description">
+            <i class="fa fa-quote-left"></i> Bla bla bla bla Bla bla bla bla Bla bla bla bla Bla bla bla bla Bla bla bla bla Bla bla bla bla Bla bla bla bla Bla bla bla bla Bla bla bla bla Bla bla bla bla Bla bla bla bla Bla bla bla bla Bla bla bla bla <i class="fa fa-quote-right"></i>
+        </div>
+    </div>
+</div>
