61a5dd78c769202586dd1111c925e85ad9377690
Vincent Theo Roque <theo.roque@newmeridian.co>
2017-04-24 09:20:44 +0800

    no message

--- resources/views/common/footer.blade.php
+++ resources/views/common/footer.blade.php
@@ -43,7 +43,7 @@
                     <h5 class="title">Follow me</h5>
                     <ul>
                         <li class="h5">
-                            <a target="_blank" href="//facebook.com/theomazing.vtr" class="badge light-blue darken-4"><i class="fa fa-facebook"></i></a>&nbsp;
+                            <a target="_blank" href="//facebook.com/vtheo.roque" class="badge light-blue darken-4"><i class="fa fa-facebook"></i></a>&nbsp;
                             <a target="_blank" href="//twitter.com/_theomazing" class="badge blue"><i class="fa fa-twitter"></i></a>&nbsp;
                             <a target="_blank" href="//www.linkedin.com/in/vincent-theo-roque-39709b9b/" class="badge light-blue darken-3"><i class="fa fa-linkedin"></i></a>&nbsp;
                             <a target="_blank" href="//www.freelancer.com/u/roquevtr.html"><img src="/images/freelancer.ico" style="width:20px;border-radius: 2px;margin-top: 1px;"></a>
